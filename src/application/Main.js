cf7958ac0ec05a3ac511d3a30e64662e18161bd5
abasalilov <abasalilov@gmail.com>
2018-06-02 02:04:34 -0700

    not confirmed

--- src/application/Main.js
+++ src/application/Main.js
@@ -1,11 +1,13 @@
 // In this file, we create a React component which contains components provided by Material-UI.
 import React, { Component } from "react";
+import { createStore } from "redux";
 import styled, { injectGlobal } from "styled-components";
 import AppBarRight from "./common/AppBar.jsx";
 import { MuiThemeProvider, getMuiTheme } from "material-ui/styles";
 import { deepOrange500 } from "material-ui/styles/colors";
-
 import { RaisedButton, Dialog, FlatButton } from "material-ui";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
 injectGlobal`
 	h1, h2 {
@@ -17,6 +19,8 @@ const Div = styled.div`
   text-align: center;
   padding-top: 200px;
 `;
+
+const store = createStore(rootReducer, { checked: 0, value: 0 });
 
 export default class Main extends Component {
   constructor(properties) {
@@ -46,31 +50,33 @@ export default class Main extends Component {
 
   render = () => (
     <MuiThemeProvider muiTheme={this.muiTheme}>
-      <AppBarRight />
-      <Div>
-        <h1>Material-UI</h1>
-        <h2>example project</h2>
-        <RaisedButton
-          label="Super Secret Password"
-          secondary={true}
-          onClick={this.handleClick}
-        />
+      <Provider store={store}>
+        <AppBarRight />
+        <Div>
+          <h1>Material-UI</h1>
+          <h2>example project</h2>
+          <RaisedButton
+            label="Super Secret Password"
+            secondary={true}
+            onClick={this.handleClick}
+          />
 
-        <Dialog
-          open={this.state.open}
-          title="Super Secret Password"
-          actions={
-            <FlatButton
-              label="Ok"
-              primary={true}
-              onClick={this.handleRequestClose}
-            />
-          }
-          onRequestClose={this.handleRequestClose}
-        >
-          1-2-3-4-5
-        </Dialog>
-      </Div>
+          <Dialog
+            open={this.state.open}
+            title="Super Secret Password"
+            actions={
+              <FlatButton
+                label="Ok"
+                primary={true}
+                onClick={this.handleRequestClose}
+              />
+            }
+            onRequestClose={this.handleRequestClose}
+          >
+            1-2-3-4-5
+          </Dialog>
+        </Div>
+      </Provider>
     </MuiThemeProvider>
   );
 }
